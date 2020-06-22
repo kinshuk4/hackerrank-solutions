726fd9f24b77626d84526b4615fcb9bf9f63936a
Kinshuk <kinshuk.ram@gmail.com>
2020-06-23 00:51:26 +0200

    Add existing solutions

---
+++ java/hackerrank/src/main/java/com/vaani/hackerrank/graph/Status.java
@@ -0,0 +1,8 @@
+package com.vaani.hackerrank.graph;
+
+/**
+ * Created by andersonkmi on 8/2/16.
+ */
+public enum Status {
+    Unvisited, Visiting, Visited;
+}
---
+++ java/hackerrank/src/main/java/com/vaani/hackerrank/misc/booking/ddosattack/KMeansParameters.java
@@ -0,0 +1,5 @@
+package com.vaani.hackerrank.misc.booking.ddosattack;
+
+public interface KMeansParameters {
+	public int NUM_ITERATIONS = 10;
+}
